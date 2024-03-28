b11055208db1d5d67462d69eeaa79847616dfc49
EmDiPalma <di.palma.emilie@protonmail.com>
2024-03-28 15:17:13 +0100

    navbar style

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import Logo from "../../assets/Logo/PNG/LOGO-LF-BLANC-SOLO.png";
 import "./Navbar.css";
 
 function Navbar() {
   const [navbarItems, setNavbarItems] = useState([]);
   const [isScrolled, setIsScrolled] = useState(false);
+  // const [isBurgerMenuOpen, setIsBurgerMenuOpen] = useState(false);
+  const location = useLocation();
 
   useEffect(() => {
     const fetchNavbarItems = async () => {
@@ -36,7 +39,7 @@ function Navbar() {
     // Ajoute un écouteur d'événements pour gérer le défilement
     window.addEventListener("scroll", handleScroll);
 
-    // Nettoye l'écouteur d'événements lors du démontage du composant
+    // Nettoie l'écouteur d'événements lors du démontage du composant
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
@@ -53,21 +56,53 @@ function Navbar() {
 
   const generatePath = (id) => idToPathMap[id] || "/";
 
+  // const toggleBurgerMenu = () => {
+  //   setIsBurgerMenuOpen(!isBurgerMenuOpen);
+  // };
+
   return (
     <div className={`Navbar${isScrolled ? " scrolled" : ""}`}>
       <a href="/">
         <img src={Logo} alt="Logo La Fabrique" />
       </a>
-      <ul>
+      {/* <ul className={`NavbarItems${isBurgerMenuOpen ? " mobile" : ""}`}> */}
+      <ul className="NavbarItems">
         {navbarItems.map((item) => (
           <li key={item.id_navbar}>
-            <a href={generatePath(item.id_navbar)}>{item.title.trim()} |</a>
+            <a
+              className={
+                location.pathname === generatePath(item.id_navbar)
+                  ? "active"
+                  : ""
+              }
+              href={generatePath(item.id_navbar)}
+            >
+              {item.title.trim()}
+            </a>{" "}
+            |
           </li>
         ))}
       </ul>
-      <div className="Lang">FR</div>
-      <div style={{ margin: "0 0.5em" }}> | </div>
-      <div className="Lang">EN</div>
+      {/* <ul
+        className={`Language NavbarItems${isBurgerMenuOpen ? " mobile" : ""}`}
+      > */}
+      <ul className="Language NavbarItems">
+        <li>
+          <a href="/" className="Lang Fr">
+            FR
+          </a>
+        </li>
+        <li>
+          <a href="/" className="Lang En">
+            EN
+          </a>
+        </li>
+      </ul>
+      {/* <div className="BurgerMenu" onClick={toggleBurgerMenu}>
+        <div className={`Bar1${isBurgerMenuOpen ? " change" : ""}`}></div>
+        <div className={`Bar2${isBurgerMenuOpen ? " change" : ""}`}></div>
+        <div className={`Bar3${isBurgerMenuOpen ? " change" : ""}`}></div>
+      </div> */}
     </div>
   );
 }
