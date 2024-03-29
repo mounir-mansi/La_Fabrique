5a0a34d3bc23a75923a21353961792db96819a42
EmDiPalma <di.palma.emilie@protonmail.com>
2024-03-29 14:12:29 +0100

    css pour home

--- frontend/src/components/Home/Presentation.jsx
+++ frontend/src/components/Home/Presentation.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "./Presentation.css";
 
 function Presentation() {
   const [section, setSection] = useState({});
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -8,8 +8,10 @@ export default function Home() {
   return (
     <div className="Home">
       <Hero logo={Logo} />
-      <Presentation />
-      <ArticleHome />
+      <div className="Home__content">
+        <Presentation />
+        <ArticleHome />
+      </div>
     </div>
   );
 }
